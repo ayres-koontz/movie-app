7ea1fefd9b04a225f23c823f00e639517d58f13a
Corey Koontz <corey.michael.koontz@gmail.com>
2021-02-26 16:15:10 -0600

    added CRUD functions

---
+++ js/movie-app.js
@@ -0,0 +1,83 @@
+"use strict"
+
+const url = 'https://pickle-dog-freeze.glitch.me/movies'
+
+// Get all movies
+const getMovies = () => fetch(url)
+    .then(res => res.json())
+    // .then(data => {
+    //     return data;
+    // }).then
+
+
+
+
+// getMovies()
+//     .then(console.log)
+//     .catch(console.error);
+
+
+
+
+// Get a movie
+const getMovie = id => fetch(`${url}/${id}`)
+    .then(res => res.json())
+    .catch(console.error);
+
+// getMovie()
+//     .then(console.log)
+//     .catch(console.error);
+
+
+
+// Create a movie
+// Input must be an object***
+const addMovie = (movie) => fetch(`${url}`, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(movie)
+})
+    .then(res => res.json())
+    .then(data => {
+        console.log(`Success: created ${JSON.stringify(data)}`);
+        return data.id; // to access the primary key of the newly created entity
+    })
+    .catch(console.error);
+
+// Modify a movie
+const editMovie = movie => fetch(`${url}/${movie.id}`, {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(movie)
+})
+    .then(res => res.json())
+    .then(data => {
+        console.log(`Success: edited ${JSON.stringify(data)}`);
+    })
+    .catch(console.error);
+
+
+
+// Delete a movie
+
+const deleteMovie = id => fetch(`${url}/${id}`, {
+    method: 'DELETE',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+    .then(res => res.json())
+    .then(() => {
+        console.log(`Success: deleted ${id}`);
+    })
+    .catch(console.error);
+
+//----------------------------------------------------------------
+
+const handleResponse = () => {
+
+}
